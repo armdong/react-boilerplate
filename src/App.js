6f4c42ae2b182346b3260de0a10e6ada1aefa7cc
wangmaodong <3003835782@qq.com>
2019-03-19 15:48:32 +0800

    Set up @babel/plugin-proposal-class-properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 
 class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  handleIncrement = () => {
+    this.setState(state => ({ count: state.count + 1 }));
+  };
+
+  handleDecrement = () => {
+    this.setState(state => ({ count: state.count - 1 }));
+  };
+
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+      <div>
+        <h1>Hello World!</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={this.handleIncrement}>Increment</button>
+        <button onClick={this.handleDecrement}>Decrement</button>
+      </div>
+    );
   }
 }
 
